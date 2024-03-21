8c94d2218723f91511c902a491236c0b0e8b203a
Christina Passafaro <christinapassafaro@pop-os.localdomain>
2024-03-21 09:52:34 -0400

    bound to computed property config now, need to bring in hiding toolbar still but error coming through now

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
-    <editor
-      api-key="no-api-key"
-      :init="{
-        height: 500,
-        menubar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount'
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-          alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help'
-      }"
-    />
+    <editor api-key="no-api-key" :init="config" />
   </div>
 </template>
 
 <script>
-import Editor from '@tinymce/tinymce-vue'
+import Editor from "@tinymce/tinymce-vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    'editor': Editor
-  }
-}
+    editor: Editor,
+  },
+  props: {
+    value: { type: Object, required: false, default: null },
+  },
+  data() {
+    return {
+      toolbar:
+        "undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help",
+    };
+  },
+  computed: {
+    config() {
+      if (this.value) {
+        return this.value;
+      } else {
+        return {
+          height: 500,
+          toolbar: this.toolbar,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          menubar: false,
+        };
+      }
+    },
+  },
+};
 </script>
