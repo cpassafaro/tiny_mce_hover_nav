0628540ffc5f376b212deb8b8275c764c6c9615b
Christina Passafaro <christinapassafaro@pop-os.localdomain>
2024-03-25 08:27:56 -0400

    brought in props and computed properties to link to

---
+++ src/components/TinyMCEWrapper.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <editor
+      api-key="0hpgd4n0xdiraxwsc7gvstlowu1x0rkqvcy8xvktjlyusly8"
+      :init="config"
+    />
+  </div>
+</template>
+
+<script>
+import Editor from "@tinymce/tinymce-vue";
+
+export default {
+  name: "tinymce-wrapper",
+  components: {
+    editor: Editor,
+  },
+  props: {
+    value: { type: Object, required: false, default: null },
+    height: { type: Number, required: false, default: null },
+    menubar: {
+      type: String,
+      required: false,
+      default: "edit insert format  view table windward help",
+    },
+    toolbar: {
+      type: String,
+      required: false,
+      default:
+        "undo redo | formatselect | fontsizeselect  | bold italic underline strikethrough removeformat | alignleft aligncenter alignright | table bullist numlist outdent indent | mathButton ",
+    },
+  },
+  data() {
+    return {
+      focus: false,
+    };
+  },
+  computed: {
+    filteredMenubar() {
+      if (this.focus) {
+        return this.menubar;
+      } else {
+        return "";
+      }
+    },
+    filteredToolbar() {
+      if (this.focus) {
+        return this.toolbar;
+      } else {
+        return "";
+      }
+    },
+    filteredHeight() {
+      if (!this.focus) {
+        return 50;
+      } else if (this.height !== null) {
+        return this.height;
+      } else {
+        return 500;
+      }
+    },
+    config() {
+      if (this.value) {
+        return this.value;
+      } else {
+        return {
+          height: this.filteredHeight,
+          visual: false,
+          menubar: this.filteredMenubar,
+          toolbar: this.filteredToolbar,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          table_advtab: false,
+          table_cell_advtab: false,
+          table_row_advtab: false,
+          table_default_attributes: {
+            class: "default",
+          },
+          font_size_formats: "8pt 10pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt",
+        };
+      }
+    },
+  },
+};
+</script>
