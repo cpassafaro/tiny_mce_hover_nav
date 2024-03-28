5d662dce9e998f10d65dcf8e790c2734ba3c1de3
Christina Passafaro <christinapassafaro@pop-os.localdomain>
2024-03-28 08:27:49 -0400

    tinymce api key not working

--- tinymce_mce_2nd_try/src/views/Home.vue
+++ tinymce_mce_2nd_try/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <TinyMCEWrapper />
+  <!-- <TinyMCEWrapper /> -->
+  <HelloWorld />
 </template>
 
 <script lang="ts" setup>
-// import HelloWorld from '@/components/HelloWorld.vue'
-import TinyMCEWrapper from "@/components/TinyMCEWrapper.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+// import TinyMCEWrapper from "@/components/TinyMCEWrapper.vue";
 </script>
